260c93900df896aca128d2c88d787d16772435a8
kpetwe <64106902+kpetwe@users.noreply.github.com>
2024-10-04 23:49:04 -0700

    updated simulation files to most recent

--- clock_ex_3.py
+++ clock_ex_3.py
@@ -1,8 +1,6 @@
-import simpy
 import numpy as np
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
-import time as tm
 from bisect import bisect_left
 
 """
@@ -40,11 +38,11 @@ class node:
     
     # To string
     def __repr__(self):
-        return "Node id: " + str(self.id) + " Current Time: " + str(self.clock.time)
+        st = f"Node id: {str(self.id)} Current Time: {self.clock.time:.4f}"
+        return st 
 
     # Send clock data
     def send(self, dest):
-        #TODO: CHECK IF SCHEDULE SAYS TO SEND DATA TO OTHER NEIGHBORS
         dest.recieve(self.id, self.clock.time)
 
     # Recieve clock data
@@ -64,16 +62,11 @@ class node:
         return False
 
     def schedule_send(self, conn):
-        #print(np.any(np.abs(np.full_like(self.send_sched, self.clock.time) - self.send_sched) <= 1e-6))
         if self.validTime(self.clock.time, self.send_sched):
-            #print("Send:", self.id, "at time", self.clock.time, end=" ")
             for nbhs in self.nbhd:
                 if nbhs in conn:
-                    #print("to:", nbhs.id, end=" ")
                     self.send(nbhs)
-            #print()
-        #else:
-            #print(self.clock.time)
+
 
     def update(self):
         if self.validTime(self.clock.time, self.send_sched):
@@ -82,7 +75,6 @@ class node:
         self.clock.increment_clock()
 
     def euler(self):
-        #print(self.id, len(self.data), self.data)
         upd = self.coef * len(self.data) * self.clock.time
         for i in self.data:
             upd -= self.coef * self.data[i]
@@ -95,12 +87,12 @@ class lcrd:
     def __init__(self, ext_nodes, sched):
         self.id = 1
         # Create nodes
-        #sched = np.arange(40023.044, 40024.120, .001)
         # node id, [start time, increment value], send schedule, update schedule, coefficient
         n2 = node(2, [40023.043, 0.0001], sched, sched, 0.487)
         n3 = node(3, [40023.057, 0.0001], sched, sched, 0.487)
         n4 = node(4, [40023.064, 0.0001], sched, sched, 0.487)
         n5 = node(5, [40023.044, 0.0001], sched, sched, 0.31)
+        
         # Add connections within nodes
         n2.add_nbhd([ext_nodes[0], n5])
         n3.add_nbhd([ext_nodes[0], n5])
@@ -108,13 +100,12 @@ class lcrd:
         n5.add_nbhd([n2, n3, n4, ext_nodes[1]])
 
         self.nodes = [n2, n3, n4, n5]
+        
         # Currently connected to ground station 
         self.gs = node(-1, [-1, -1], -1, -1, -1)
         
     def recieve(self, other_id, other_time):
-        #print("lcrd recieve", other_id)
         if (other_id == 10):
-            #print(self.gs.id)
             self.gs.recieve(other_id, other_time)
         elif(other_id == 100):
             # node 5
@@ -159,17 +150,58 @@ class network:
         self.n10.add_nbhd([self.lcrd])
         self.n100.add_nbhd([self.lcrd])
     
+    # Set up lists for plotting results
+    def plotter_setup(self, nodes):
+        plots = []
+        for i in range(len(nodes)):
+            plots.append([nodes[i].clock.time])
 
-    def run(self):
+        return plots
+    
+    # Update lists for plotting results
+    def plotter_upd(self, plots):
+        plots[0].append(self.n100.clock.time)
+        plots[1].append(self.n10.clock.time)
+        plots[2].append(self.lcrd.nodes[0].clock.time)
+        plots[3].append(self.lcrd.nodes[1].clock.time)
+        plots[4].append(self.lcrd.nodes[2].clock.time)
+        plots[5].append(self.lcrd.nodes[3].clock.time)
+
+    # Plot time differences at each iteration
+    def plotter_res(self, plots, c):
+        # Plotter
+        for i in range(len(c)):
+            avg = np.average([plots[0][i], plots[1][i], plots[2][i], plots[3][i], plots[4][i], plots[5][i]])
+            for j in range(len(plots)):
+                plots[j][i] = plots[j][i] - avg
+
+        plt.rcParams['font.size'] = 24
+        plt.plot(c, plots[0])
+        plt.plot(c, plots[1], color='maroon')
+        plt.plot(c, plots[2], color='mediumpurple')
+        plt.plot(c, plots[3], color='hotpink')
+        plt.plot(c, plots[4], color='darkorange')
+        plt.plot(c, plots[5], color='darkgreen')
+        plt.legend(['n100', 'n10', 'n2', 'n3', 'n4', 'n5'])
+        plt.xlabel("Iteration Count")
+        plt.ylabel("Time Difference (Seconds)")
+        plt.title("Difference from Mean Clock Time vs. Iteration Count (Ground Station 4)")
+        plt.show()
+
+    # Runs the simulation
+    def run(self, dur):
+        # Plotter
+        c = [0]
+        i = 0
+        plots = self.plotter_setup([self.n100, self.n10, self.lcrd.nodes[0], self.lcrd.nodes[1], self.lcrd.nodes[2], self.lcrd.nodes[3]])
         start = timer()
         time = timer()
-        #for i in range(38):
-        # Run for approximately .1 second
-        while (time - start < .1): 
-            #print(time)
-            #st = timer()
+        while (np.abs(time - start) <= dur):
+            i += 1
+            c.append(i)
             lc_time = int(time) % 60
             self.lcrd.run(lc_time)
+            
             # Send data
             self.n10.schedule_send([self.lcrd])
             self.n100.schedule_send([self.lcrd])
@@ -179,16 +211,11 @@ class network:
             self.n100.update()
             for node in self.lcrd.nodes:
                 node.update()
-            #print(timer() - st)
-            #self.n100.euler()
-            #self.n100.clock.increment_clock()
-            #self.n10.euler()
-            #self.n10.clock.increment_clock()
-            #for node in self.lcrd.nodes:
-                #node.euler()
-                #node.clock.increment_clock()
-            tm.sleep(.0001)
-            time = int(timer())
+
+            self.plotter_upd(plots)
+            time = timer()
+
+        self.plotter_res(plots, c)
             
     # To String for network        
     def __repr__(self):
@@ -199,60 +226,8 @@ class network:
         return string
         
 
-
-## NO STRATUM
-"""
-n10 = node(10, [40023.054, 0.001], -1, -1, 0.5 * np.array([1, -1]))
-n2 = node(2, [40023.043, 0.001], -1, -1, 0.4999 * np.array([[-1, 2, -1], [0, 1, -1]]))
-n3 = node(3, [40023.057, 0.001], -1, -1, 0.4999 * np.array([[-1, 2, -1], [0, 1, -1]]))
-n4 = node(4, [40023.064, 0.001], -1, -1, 0.4999 * np.array([[-1, 2, -1], [0, 1, -1]]))
-n5 = node(5, [40023.044, 0.001], -1, -1, 0.31 * np.array([-1, -1, -1, 4, -1]))
-n100 = node(100, [40023.055, 0.001], -1, -1, 0.5 * np.array([-1, 1]))
-
-print(n10)
-"""
-
 n = network()
 print(n)
-n.run()
+# Run for approximately .005 seconds
+n.run(.005)
 print(n)
-
-
-"""
-# Represent switching between operational satellite by using computer time
-def updateStation(time):
-    gs = -1
-    curr = int(time) % 60
-    #curr = int(timer()) % 60
-    print(curr)
-    if (curr < 20 and gs != 2):
-        gs = 2
-        print("Current Ground Station Available:", gs)
-    elif (curr >= 20 and curr < 40 and gs != 3):
-        gs = 3
-        print("Current Ground Station Available:", gs)
-    elif (curr >= 40 and gs != 4):
-        gs = 4
-        print("Current Ground Station Available:", gs)    
-    #time.sleep(1)
-"""
-
-
-"""
-# Use this iteration to increment clocks
-# change dt for different rates
-for i in range(1):
-    #start = timer()
-    # around 2e-5 to 3e-5 to increment all the clocks
-    # So make sure you sleep for a value greater than that
-    n10.clock.increment_clock()
-    n2.clock.increment_clock()
-    n3.clock.increment_clock()
-    n4.clock.increment_clock()
-    n5.clock.increment_clock()
-    n100.clock.increment_clock()
-    #end = timer()
-    #print(end - start)
-    # .001 sec / sec
-    time.sleep(1)
-"""
